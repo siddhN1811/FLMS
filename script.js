042439d2c9b797b39afa4269854f217571317aa5
siddhN1811 <siddhanthgn@yahoo.com>
2023-02-21 01:40:19 +0530

    alertBtn

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-let mapOptions = {
-    center:[51.958, 9.141],
-    zoom:10
+function myFunction(){
+    alert("Hey! your query is submitted!")
 }
-
-
-let map = new L.map('mapUpdate' , mapOptions);
-
-let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-map.addLayer(layer);
-
-let marker = new L.Marker([51.958, 9.141]);
-marker.addTo(map);

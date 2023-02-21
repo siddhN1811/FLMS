ab52250a01d57c219ce7e1c2bdfcb19639a6634d
Shiab098 <86104652+Shiab098@users.noreply.github.com>
2023-02-21 13:16:51 +0530

    weather

--- Prediction/weather_app/views.py
+++ Prediction/weather_app/views.py
@@ -4,24 +4,26 @@ from django.shortcuts import render
 
 import requests
 
-def get_weather(kurla):
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={kurla}&appid={'086b10858db32a1f21b26efeef0ae98b'}&units=metric"
+def get_weather(city):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q=mumbai&units=imperial&appid=086b10858db32a1f21b26efeef0ae98b"
     response = requests.get(url)
     data = response.json()
     weather = {
-        'city': kurla,
+        'city': city,
         'temperature': data['main']['temp'],
         'description': data['weather'][0]['description'],
         'icon': data['weather'][0]['icon'],
     }
+    weather['temperature'] = ((weather['temperature'] -32)*5)/9
     return weather
 
 def weather(request):
-    city = request.GET.get('city')
-    if city:
-        weather = get_weather(city)
-    else:
-        weather = None
+    # city = request.GET.get('city')
+    # if city:
+    weather = get_weather('Kurla')
+    # else:
+        # weather = None
+    print(weather['city'])
     context = {
         'weather': weather,
     }

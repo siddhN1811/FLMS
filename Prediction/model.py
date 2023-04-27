acbfa113e90dc5cb6270cb8ac6e94fd97da8deb1
siddhN1811 <siddhanthgn@yahoo.com>
2023-04-27 21:10:36 +0530

    prediction

--- Prediction/model.py
+++ Prediction/model.py
@@ -7,8 +7,12 @@ def predict(values):
     precip = pickled_model.predict(values)
 
     # print(pickled_model.predict(values))  # debugging purpose
+    print(int(precip))
+    if int(precip) ==0:
+        
+        return 1
+    return int(precip)
 
-    return precip
 
 # x = [[28.0, 25.9, 26.7, 29.0, 95.0, 272.6]]  #TEST DATA - 'tempmax', 'tempmin','temp','feelslike', 'humidity','winddir'
 # x2 = [[33.98, 31.93, 33.98, 38.0388889, 49,260]]
